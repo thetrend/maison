ba7dd05655bcba328bb553a37451a8b6601df08f
Grace de la Mora <grace.g.delamora@gmail.com>
2022-03-29 22:14:10 -0400

    Cleaning up the url helper take 2

--- functions/auth/index.ts
+++ functions/auth/index.ts
@@ -1,10 +1,14 @@
 import { Handler } from '@netlify/functions';
 let urlHelper = require('../utils/urlHelper');
+
 const handler: Handler = async (event) => {
   let ApiUrl = new urlHelper.ApiUrl(event);
+
+  const { path, segments, func, endpoint } = ApiUrl;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ segments: (ApiUrl.segments).filter(segment => segment !== ApiUrl.func), funcName: ApiUrl.func, endpoint: ApiUrl.endpoint })
+    body: JSON.stringify({ segments: segments.filter(segment => segment !== func), funcName: func, endpoint, path })
   };
 };
 
